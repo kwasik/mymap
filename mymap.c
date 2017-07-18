407b9765cdb9034ac8cb318d0d903de2d43ee0c2
Krystian Wasik <wasik.krystian@gmail.com>
2017-07-18 20:56:25 +0200

    Added mymap.c with empty definitions

---
+++ mymap.c
@@ -0,0 +1,27 @@
+/*
+ * mymap.c
+ *
+ *  Created on: 18.07.2017
+ *      Author: krystian
+ */
+
+#include "mymap.h"
+#include "stddef.h"
+
+/* Exported functions ------------------------------------------------------- */
+int mymap_init(map_t *map) {
+	return MYMAP_ERR;
+}
+
+int mymap_dump(map_t *map) {
+	return MYMAP_ERR;
+}
+
+void *mymap_mmap(map_t *map, void *vaddr, unsigned int size, unsigned int flags,
+		void *o) {
+	return NULL;
+}
+
+void mymap_munmap(map_t *map, void *vaddr) {
+
+}
